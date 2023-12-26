52b1392199b22489a07c801cbfb856d5d3d9c1ed
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2023-12-26 22:33:30 +0100

    finished
    quick sort 50 min semi hard but i wanted to really grasp everything so it took me a bit longer

--- quicksort.js
+++ quicksort.js
@@ -1,20 +1,67 @@
 function quicksort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
 
-  // Check if the input is length 1 or less
-    // If so, it's already sorted: return
+  const pivot = arr[0];
+  const less = [];
+  const equal = [];
+  const greater = [];
 
-  // Pick the first value as the pivot
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      less.push(arr[i]);
+    } else if (arr[i] === pivot) {
+      equal.push(arr[i]);
+    } else {
+      greater.push(arr[i]);
+    }
+  }
 
-  // Orient the pivot so that...
-      // every number smaller than the pivot is to the left
-      // every number larger (or equal) than the pivot is to the right
+  const sortedLess = quicksort(less);
+  const sortedGreater = quicksort(greater);
 
-  // Recursively sort the left
-  // Recursively sort the right
-
-  // Return the left, pivot and right in sorted order
-
+  return sortedLess.concat(equal, sortedGreater);
 }
 
+// Example usage:
+const unsortedArray = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+const sortedArray = quicksort(unsortedArray.slice()); // Create a copy to avoid mutating the original array
+console.log(sortedArray);
+
+
+
+
+function inPlaceQuickSort (arr, low = 0, high = arr.length -1){
+  if(low < high){
+    const partitionIndex = partition(arr,low,high)
+
+    inPlaceQuickSort(arr,low, partitionIndex -1)
+    inPlaceQuickSort(arr,partitionIndex +1 ,high)
+
+    
+  }
+  return arr;
+}
+
+function partition (arr, low, high){
+  const pivot = arr[high];
+  let i = low -1
+
+
+  for(let j = low; j < high; j++){
+    if(arr[j] < pivot){
+      i++;
+      [arr[i],arr[j]] = [arr[j],arr[i]];
+    }
+  }
+  [arr[i+1],arr[high]] = [arr[high],arr[i+1]]
+  return i+1;
+}
+
+const unsortedArrayInPlace = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+const sortedArrayInPlace = inPlaceQuickSort(unsortedArrayInPlace); // Create a copy to avoid mutating the original array
+console.log(sortedArrayInPlace);
+console.log(unsortedArrayInPlace);
 
 module.exports = [quicksort];
